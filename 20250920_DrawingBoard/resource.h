a9b419942a3432e94ba62aa675fe8cb1446c864a
KimTaeJung <xownd200225@gmail.com>
2025-12-01 15:26:38 +0900

    first commit

---
+++ 20250920_DrawingBoard/resource.h
@@ -0,0 +1,47 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// 20250920_DrawingBoard.rc에서 사용되고 있습니다.
+//
+#define IDC_MYICON                      2
+#define IDD_MY20250920DRAWINGBOARD_DIALOG 102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_MY20250920DRAWINGBOARD      107
+#define IDI_SMALL                       108
+#define IDC_MY20250920DRAWINGBOARD      109
+#define IDR_MAINFRAME                   128
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_32774                        32774
+#define ID_SHAPE_LINE                   32775
+#define ID_SHAPE_RECT                   32776
+#define ID_SHAPE_ELLIPSE                32777
+#define ID_SHAPE_FREE                   32778
+#define ID_32779                        32779
+#define ID_32780                        32780
+#define ID_32781                        32781
+#define ID_THICKNESS_1                  32782
+#define ID_THICKNESS_5                  32783
+#define ID_THICKNESS_10                 32784
+#define ID_32785                        32785
+#define ID_32786                        32786
+#define ID_32787                        32787
+#define ID_COLOR_BLACK                  32788
+#define ID_COLOR_WHITE                  32789
+#define ID_COLOR_RED                    32790
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32791
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
